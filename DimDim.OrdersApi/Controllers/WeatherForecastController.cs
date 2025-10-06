e54eef6f559f836f6fb92410534d7ccf7dfd3fbb
João Paulo Moreira <moreira.joaopaulo1993@gmail.com>
2025-10-06 20:27:11 -0300

    Adição do WeatherForecastController.cs para evitar erro de build

---
+++ DimDim.OrdersApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DimDim.OrdersApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("WeatherForecastController ativo, mas não utilizado.");
+        }
+    }
+}
